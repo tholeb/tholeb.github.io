8afed57d9e91a87e2208474c74e4e370eaf99598
tholeb <43311443+tholeb@users.noreply.github.com>
2022-10-16 17:06:42 +0200

    Fix tailwind not working

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
-export default {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	important: "#root",
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+	important: '#root',
 	theme: {
 		extend: {},
 	},
