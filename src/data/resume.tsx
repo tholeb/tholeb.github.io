af4f02ff8ce4f6fff171e340d27df69f95d67c6e
tholeb <43311443+tholeb@users.noreply.github.com>
2024-09-09 16:15:39 +0200

    Update data

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -3,15 +3,13 @@ import { HomeIcon, NotebookIcon } from "lucide-react";
 import { Icons } from "@/components/icons";
 
 export const DATA = {
-  name: "Thomas LEBRETON",
-  initials: "TL",
+  name: "Thomas L.",
+  initials: "( ͡° ͜ʖ ͡°)",
   url: "https://tholeb.fr",
   location: "France",
   locationLink: "",
-  description:
-    "Passionate student in systems administration and emerging technologies, currently in my fifth year at Polytech Nantes.",
-  summary:
-    "Self taught system administrator and software developer with a passion for emerging technologies. I have experience with a wide range of technologies and programming languages. I am a quick learner and I am always looking to improve my skills.",
+  description: "Passionate student in systems administration and emerging technologies, currently in my fifth year at Polytech Nantes.",
+  summary: "Self taught system administrator and software developer with a passion for emerging technologies. I have experience with a wide range of technologies and programming languages. I am a quick learner and I am always looking to improve my skills.",
   avatarUrl: "",
   skills: [
 	"Ansible",
@@ -62,7 +60,14 @@ export const DATA = {
    	{
       company: "Thales NL",
       href: "#",
-      badges: [],
+      badges: [
+		"Linux",
+		"Ansible",
+		"Ansible Tower",
+		"Helm",
+		"Kubernetes",
+		"OpenFeature",
+	  ],
       location: "Engelo, Netherlands",
       title: "Research project on Kubernetes, OpenFeature and Helm",
       logoUrl: "https://pbs.twimg.com/profile_images/1609851367299874819/Jr479xkK_400x400.jpg",
@@ -72,36 +77,49 @@ export const DATA = {
         "Research project on Kubernetes, OpenFeature and Helm. I had to deploy a kuberntes cluster implementing OpenFeature in a Express application. I used Unleash as a provider.",
     },
     {
-      company: "CNAM",
-      badges: [],
+      company: "Caisse Nationale de l'Assurance Maladie",
+      badges: [
+		"linux",
+		"Ansible",
+		"PostgreSQL",
+		"Rundeck",
+	  ],
       href: "#",
-      location: "Angers, France",
-      title: "Sysadmin intern",
-      logoUrl: "	https://kamelecom.fr/wp-content/uploads/2023/03/cpam_300x3005979ee4fbd701.jpg",
-      start: "",
-      end: "",
-      description:
-        "",
-    },
-    {
-      company: "CNAM",
-      href: "#",
-      badges: [],
       location: "Angers, France",
       title: "Sysadmin intern",
       logoUrl: "https://kamelecom.fr/wp-content/uploads/2023/03/cpam_300x3005979ee4fbd701.jpg",
-      start: "",
-      end: "",
-      description:
-        "",
+      start: "June 2023",
+      end: "August 2023",
+      description: "Project management, production launch and management of the manufacturer's recipe by a service provider.\
+	  	Created a playbook to migrate a rundeck to another server with a PSQL database.\
+		Migrated servers from CentOS7 to AlmaLinux8 with a kickstart, while keeping some critical data using LVM.\
+		Discovered and used Ansible Automation Platform. I had to automate my rundeck migration playbook as a Proof Of Concept.\
+		Updated a GitLab using a playbook.",
+    },
+    {
+      company: "Caisse Nationale de l'Assurance Maladie",
+      href: "#",
+      badges: [
+		"Linux",
+		"Ansible",
+		"Cockpit",
+		"redfish",
+	  ],
+      location: "Angers, France",
+      title: "Sysadmin intern",
+      logoUrl: "https://kamelecom.fr/wp-content/uploads/2023/03/cpam_300x3005979ee4fbd701.jpg",
+      start: "April 2022",
+      end: "June 2022",
+      description: "Automated hardware configuration (BIOS) using Ansible for a CentOS 7 server using redfish standard and iDRAC boards for Dell (Tower and Rack) server.\
+		Installed and configured an IT HMI (Red Hat’s Cockpit) to help manage and debug the system for technicians in data centres.",
     },
   ],
   education: [
     {
       school: "Polytech Nantes",
       href: "#",
-      degree: "Masters in Computer Science",
-      logoUrl: "",
+      degree: "Master in Computer Science",
+      logoUrl: "https://media.licdn.com/dms/image/v2/C4E0BAQGGV9BX_tTmIw/company-logo_200_200/company-logo_200_200/0/1631319858106?e=2147483647&v=beta&t=-pGskuKmeThHwEce9NCFEvg5yu5i4ovY8ifFl8xNBDM",
       start: "2022",
       end: "2025",
     },
@@ -109,7 +127,7 @@ export const DATA = {
       school: "University of Nantes",
       href: "#",
       degree: "Technical Degree in Computer Science",
-      logoUrl: "",
+      logoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9b5X1NY6umcesefcL3o89t1Qm9xNBjBxaNA&s",
       start: "2020",
       end: "2022",
     },
@@ -134,27 +152,26 @@ export const DATA = {
         },
 		{
           type: "Discord",
-          href: "https://visionrp.fr",
+          href: "https://discord.com/invite/visionrp",
           icon: <Icons.discord className="size-3" />,
         }
 	  ],
-      image: "",
+      image: "https://img.tholeb.fr/u/3178abce-d5d8-493c-9ada-6e961be27a1b.png",
       video: "",
     },
 	{
-      title: "Raspberry Pi 4",
-      href: "",
+      title: "Home server (Raspberry Pi 4)",
+      href: "#",
       dates: "2020 - Today",
       active: true,
-      description:
-        "",
+      description: "I have a home server that I use for various purposes, such as containers, Nextcloud, a VPN server, and more.",
       technologies: [
         "Ansible",
         "Linux",
         "podman",
       ],
       links: [],
-      image: "",
+      image: "https://download.logo.wine/logo/Raspberry_Pi/Raspberry_Pi-Logo.wine.png",
       video: "",
     },
   ],
